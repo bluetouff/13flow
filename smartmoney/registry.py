c501cd681eb3ad8a012f4008ea3e15bb9a8f5cfc
bluetouff <admin@toonux.com>
2026-06-21 13:48:32 +0200

    registry: 8->38 funds, verified CIKs against EDGAR
    - Add 30 superinvestors (activists, deep-value, growth, quality)
    - Fix wrong CIKs: Eminence 1107310, Whale Rock 1387322, D1 1747057,
      Light Street 1569049, FPA/Romick 1377581 (First Pacific Advisors)
    - Repoint entity migrations: Greenlight->DME Capital 1489933,
      JANA->JANA Partners Management 1998597
    - All CIKs verified via run.py --verify against EDGAR entity names

--- smartmoney/registry.py
+++ smartmoney/registry.py
@@ -31,6 +31,43 @@ SUPERINVESTORS: list[Fund] = [
     Fund("Duquesne FO", "Stanley Druckenmiller", "1536411", "Duquesne Family Office"),
     Fund("Himalaya Capital", "Li Lu", "1709323", "Himalaya Capital Management"),
     Fund("Appaloosa", "David Tepper", "1656456", "Appaloosa LP"),
+    Fund("Greenlight",     "David Einhorn",   "1489933",         "DME Capital Management"),
+    Fund("Lone Pine",      "Stephen Mandel",  "1061165",         "Lone Pine Capital"),
+    Fund("Third Point",    "Dan Loeb",        "1040273",         "Third Point LLC"),
+    Fund("Akre Capital",   "Chuck Akre",      "1112520",         "Akre Capital Management"),
+    # --- value / concentrés (riches en signal) ---
+    Fund("Value Act",       "Jeff Ubben/ValueAct", "1418814", "ValueAct Holdings"),
+    Fund("Oakmark/Harris",   "Bill Nygren",         "813917", "Harris Associates"),
+    # --- growth / tech (gros déposants, utiles au consensus) ---
+    Fund("Tiger Global",     "Chase Coleman",       "1167483", "Tiger Global Management"),
+    Fund("Coatue",           "Philippe Laffont",    "1135730", "Coatue Management"),
+    Fund("Viking Global",    "Andreas Halvorsen",   "1103804", "Viking Global Investors"),
+    # --- activistes / macro ---
+    Fund("Icahn",            "Carl Icahn",          "0921669", "Icahn Capital"),
+    Fund("Trian",            "Nelson Peltz",        "1345471", "Trian Fund Management"),
+    Fund("Soros",            "Soros (FO)",          "1029160", "Soros Fund Management"),
+    # --- value / deep value ---
+    Fund("Fairholme",        "Bruce Berkowitz",     "1056831", "Fairholme Capital"),
+    Fund("Oaktree",          "Howard Marks",        "949509",  "Oaktree Capital Management"),
+    Fund("FPA / Romick",     "Steven Romick",       "1377581", "First Pacific Advisors"),
+    Fund("Dodge & Cox",      "Dodge & Cox",         "200217",  "Dodge & Cox"),
+    Fund("Davis Advisors",   "Chris Davis",         "1036325", "Davis Selected Advisers"),
+    # --- quality / compounders ---
+    Fund("Ruane Cunniff",    "Sequoia Fund",        "89043",   "Ruane Cunniff"),
+    Fund("Gardner Russo",    "Tom Russo",           "860643",  "Gardner Russo & Quinn"),
+    Fund("TCI",              "Chris Hohn",          "1647251", "TCI Fund Management"),
+    Fund("Polen Capital",    "Polen Capital",       "1034524", "Polen Capital Management"),
+    # --- activistes / event-driven ---
+    Fund("Elliott",          "Paul Singer",         "1791786", "Elliott Investment Management"),
+    Fund("Starboard Value",  "Jeff Smith",          "1517137", "Starboard Value"),
+    Fund("JANA Partners",    "Barry Rosenstein",    "1998597", "JANA Partners Management"),
+    Fund("Sachem Head",      "Scott Ferguson",      "1582090", "Sachem Head Capital"),
+    Fund("Eminence",         "Ricky Sandler",       "1107310", "Eminence Capital"),
+    # --- growth / tech / multistrat ---
+    Fund("Whale Rock",       "Alex Sacerdote",      "1387322", "Whale Rock Capital"),
+    Fund("Altimeter",        "Brad Gerstner",       "1541617", "Altimeter Capital"),
+    Fund("D1 Capital",       "Dan Sundheim",        "1747057", "D1 Capital Partners"),
+    Fund("Light Street",     "Glen Kacher",         "1569049", "Light Street Capital"),
 ]
 
 
